459b2d4d003dbab73cb708ff88218c11582d9129
aycaRichard12 <aycarichard7@gmail.com>
2025-04-15 05:19:21 -0400

    registro cliente

--- index.js
+++ index.js
@@ -8,30 +8,35 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
 const cochesRouter = require('./routes/coches');
-const estadoCocheRouter = require('./routes/estado_vehiculo');
-const marcaCocheRouter = require('./routes/marca');
-const tipoConbustibleCocheRouter = require('./routes/tipo_conbustible');
-const modeloCocheRouter = require('./routes/modelo');
-const tipoCocheRouter = require('./routes/tipo_vehiculo');
-const RegistroCocheRouter = require('./routes/coche');
-const precioCocheRouter = require('./routes/precio');
-const reservasRouter = require('./routes/reservas');
-const busquedaRouter = require('./routes/busqueda');
+const usuariosRouter = require('./routes/Usuarios')
+const estadoCocheRouter = require('./routes/estado_vehiculo')
+const marcaCocheRouter = require('./routes/marca')
+const tipoConbustibleCocheRouter = require('./routes/tipo_conbustible')
+const modeloCocheRouter = require('./routes/modelo')
+const tipoCocheRouter = require('./routes/tipo_vehiculo')
+const RegistroCocheRouter = require('./routes/coche')
+const precioCocheRouter = require('./routes/precio')
+const reservasRouter = require('./routes/reservas')
+const busquedaRouter = require('./routes/busqueda')
+const cocheimgRouter = require('./routes/cocheimg'); // 🆕 Importa la nueva ruta de imágenes
+const clienteRouter = require('./routes/clientes')
 
-// Usar rutas
 app.use('/reservas', reservasRouter);
-app.use('/coches', cochesRouter);
-app.use('/estadocoche', estadoCocheRouter);
-app.use('/marca', marcaCocheRouter);
-app.use('/tipocombustible', tipoConbustibleCocheRouter);
-app.use('/modelo', modeloCocheRouter);
-app.use('/tipo', tipoCocheRouter);
-app.use('/coche', RegistroCocheRouter);
-app.use('/precio', precioCocheRouter);
-app.use('/buscar', busquedaRouter);
+app.use('/coches',cochesRouter);
+app.use('/estadocoche',estadoCocheRouter);
+app.use('/marca',marcaCocheRouter);
+app.use('/tipocombustible',tipoConbustibleCocheRouter);
+app.use('/modelo',modeloCocheRouter);
+app.use('/tipo',tipoCocheRouter);
+app.use('/coche',RegistroCocheRouter);
+app.use('/precio',precioCocheRouter);
+app.use('/buscar',busquedaRouter)
+app.use('/usuarios', usuariosRouter);
+app.use('/cocheimg',cocheimgRouter); // 🆕 Usar la nueva ruta aquí
+app.use('/cliente',clienteRouter); // 🆕 Usar la nueva ruta aquí
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+
